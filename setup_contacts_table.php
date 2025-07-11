3f72439df66d7b93ca2b073c1898fd45755d6369
gabignatova <gabignatova@gmail.com>
2025-07-11 10:58:33 +0300

    changes

--- setup_contacts_table.php
+++ setup_contacts_table.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Database configuration
 $host = 'localhost';
 $dbname = 'petstore_db';
@@ -44,4 +48,3 @@ if (!$tableExists) {
 
 // Close the connection
 $conn->close();
-?>
